f317a44c6318f0f5735f0f67f0d030fd078309cf
kabradshaw1 <itllbok82@gmail.com>
2022-08-06 22:02:22 -0400

    3.1.5

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+var playerName = window.prompt("what is your robot's name?")
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
+console.log(playerName);
+console.log("This longs a string, good for leaving yourself a message");
+// this will do mayh and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
