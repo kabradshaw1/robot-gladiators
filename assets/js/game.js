cfe80c5d07e0b405bad6bdf43f723b0d2539b8d1
kabradshaw1 <itllbok82@gmail.com>
2022-08-14 11:56:15 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,13 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+};
+
+var enemyHealth = randomNumber(40,60);
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
@@ -21,16 +27,18 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
-      // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
-        playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
+        playerName + ' attacked ' + enemyName + ' for ' + damage + ". " + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
   
       // check enemy's health
@@ -45,10 +53,12 @@ var fight = function(enemyName) {
         window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
       }
   
-      // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+      playerHealth = Math.max(0, playerHealth - damage);
+
       console.log(
-        enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
+        enemyName + ' attacked ' + playerName + ' for ' + damage + ". " + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
   
       // check player's health
@@ -61,6 +71,8 @@ var fight = function(enemyName) {
       }
     } // end of while loop
   }; // end of fight function;
+
+// function to generate a random numeric value
 
 //function to start a new game
 var startGame = function (){
